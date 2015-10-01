606137e7363f575eddb61053c2e3f036cbaaca17
Hugo Aguero <hugo.aguero@drivedominion.com>
2015-10-01 16:43:53 -0600

    Initial Commit

---
+++ MovieDemo/NavigationControllerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NavigationControllerDemo
+//
+//  Created by Hugo Aguero on 9/30/15.
+//  Copyright © 2015 Hugo Aguero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate , UINavigationControllerDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
